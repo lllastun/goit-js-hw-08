8e9c5bb0bc92abfe8acf3ba29ff5f8bff928de9a
lllastun <lllastun@gmail.com>
2023-04-25 23:30:52 +0300

    gallery-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
 // Add imports above this line
+
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// Додатковий імпорт стилів
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+function createLiElem(image) {
+  const { preview: smallImg, original: bigImg, description: descriptionImg } = image;
+  return `<li class="gallery__item"> 
+      <a class='gallery__link' href="${bigImg}">
+        <img class="gallery__image" src="${smallImg}" alt="${descriptionImg}" />
+      </a> 
+    </li>
+     `;
+}
+
+const galleryArray = [];
+const ulElement = document.querySelector(".gallery");
+const options = {
+  captionsData: "alt",
+  captionDelay: 250,
+};
+
+for (let i = 0; i < galleryItems.length; i++) {
+  galleryArray.push(createLiElem(galleryItems[i]));
+}
+const insertedString = galleryArray.join("");
+ulElement.insertAdjacentHTML("afterbegin", insertedString);
+
+let gallery = new SimpleLightbox(".gallery a", options);
